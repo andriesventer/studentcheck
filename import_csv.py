1eb04102ad1119eec5f263343affbb2a0224475d
andriesventer <andriesv@gmail.com>
2026-05-24 06:46:37 +0200

    cycle start and end dates added

--- import_csv.py
+++ import_csv.py
@@ -4,11 +4,12 @@ api_key="6RDGH9fMHHTvCiQbmBKRnat77Y49vS0p"
 
 
 
-def enrolment_check(cycle_name, idnr, programme_code, enrolled, api_key):
+def enrolment_check(cycle_startdate, cycle_enddate, idnr, programme_code, enrolled, api_key):
     url = "https://studdup.tvet.org.za/enrolment-check"
     payload = {
-        "cycle_name": cycle_name,
         "idnr": idnr,
+        "cycle_startdate": cycle_startdate,
+        "cycle_enddate": cycle_enddate,
         "programme_code": programme_code,
         "enrolled": enrolled
     }
@@ -27,23 +28,24 @@ with open('export.csv', 'r') as f,open('results.txt', 'w') as results_file:
     # Assuming the first line contains the header
     header = lines[0].strip().split(',')
     # Write the header to the results file
-    results_file.write(f"IdNr,Cycle,ProgrammeCode, Enrolled,Result,Duplicated\n")
+    results_file.write(f"IdNr,CycleStart,CycleEnd,ProgrammeCode,Enrolled,Result,Duplicated\n")
     # Assuming the second line contains the data
     for line in lines[1:]:
         data = line.strip().split(',')
-        cycle_name = data[0]
-        idnr = data[1] 
-        programme_code = data[2]
-        enrolled = data[3]
-        result = enrolment_check(cycle_name, idnr, programme_code, enrolled, api_key)
+        cycle_startdate = data[0]
+        cycle_enddate = data[1]
+        idnr = data[2]
+        programme_code = data[3]
+        enrolled = data[4]
+        result = enrolment_check(cycle_startdate, cycle_enddate, idnr, programme_code, enrolled, api_key)
         # Write the result to the file
         if not result:
             result ="Error - No response from server"
         elif isinstance(result, tuple):
             result, duplicates = result
         
-        print(f"IDNR: {idnr}, Cycle: {cycle_name}, Programme Code: {programme_code}, Enrolled: {enrolled} ,Result: {result}, Duplicated: {duplicates if len(duplicates)>0 else 'N/A'}")
-        results_file.write(f"{idnr},{cycle_name},{programme_code},{enrolled},{result},{duplicates if len(duplicates)>0 else 'N/A'}\n")
+        print(f"IDNR: {idnr}, Start: {cycle_startdate}, End: {cycle_enddate}, Programme Code: {programme_code}, Enrolled: {enrolled}, Result: {result}, Duplicated: {duplicates if len(duplicates)>0 else 'N/A'}")
+        results_file.write(f"{idnr},{cycle_startdate},{cycle_enddate},{programme_code},{enrolled},{result},{duplicates if len(duplicates)>0 else 'N/A'}\n")
 
 
 
